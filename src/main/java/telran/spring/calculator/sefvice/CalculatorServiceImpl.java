ba430f6aa7e9a77516266a53adfb6f3602c387e1
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-05 17:58:04 +0200

    HW #56 Calculator Web Service

---
+++ src/main/java/telran/spring/calculator/sefvice/CalculatorServiceImpl.java
@@ -0,0 +1,31 @@
+package telran.spring.calculator.sefvice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+	@Override
+	public double multiply(double op1, double op2) {
+		
+		return op1 * op2;
+	}
+
+	@Override
+	public double sum(double op1, double op2) {
+		
+		return op1 + op2;
+	}
+
+	@Override
+	public double substract(double op1, double op2) {
+		return op1-op2;
+	}
+
+	@Override
+	public double divide(double op1, double op2) {
+		
+		return op1 / op2;
+	}
+
+}
